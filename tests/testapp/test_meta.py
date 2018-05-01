736c6b0f066dc02d4036733612673cf0549771d5
Matthias Kestenholz <mk@feinheit.ch>
2018-05-01 11:44:42 +0200

    I like pytest, but I like the tox.ini interface more

---
+++ tests/testapp/test_meta.py
@@ -0,0 +1,32 @@
+from django import test
+from django.utils.functional import lazy
+
+from feincms3_meta.utils import meta_tags
+
+
+class MetaTest(test.TestCase):
+    def test_meta(self):
+        request = test.RequestFactory().get('/')
+        self.assertEqual(
+            str(meta_tags(request=request)),
+            '''\
+<meta property="og:type" content="website">
+  <meta name="description" content="">''')
+
+        lazy_url = lazy(lambda: '/', str)()
+        self.assertEqual(
+            str(meta_tags(url=lazy_url, request=request)),
+            '''\
+<meta property="og:type" content="website">
+  <meta property="og:url" content="http://testserver/">
+  <meta name="description" content="">''')
+
+        self.assertEqual(
+            str(meta_tags(
+                request=request,
+                defaults={'title': 'stuff'},
+                title=None,
+            )),
+            '''\
+<meta property="og:type" content="website">
+  <meta name="description" content="">''')
