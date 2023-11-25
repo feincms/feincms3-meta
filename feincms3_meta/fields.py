32047868ad8f08c5672e6db9a7c6ce827810cfdb
York Schickl <ys@feinheit.ch>
2023-11-25 11:24:03 +0100

    coerce option for structured data properties

--- feincms3_meta/fields.py
+++ feincms3_meta/fields.py
@@ -2,10 +2,25 @@ from django.conf import settings
 
 
 class StructuredDataProperty:
-    def __init__(self, property, value=None, keyword=None):
+    def __init__(self, property, value=None, keyword=None, coerce=None):
         self.property = property
         self.value = value
         self.keyword = keyword
+        self.coerce = coerce
+
+    def to_ld_dict(self, obj):
+        data = {}
+        value = self.value(obj) if callable(self.value) else self.value
+        value = self.coerce(obj, value) if self.coerce else value
+        if not value:
+            return data
+
+        if self.keyword:
+            data[self.property] = {self.keyword: value}
+        else:
+            data[self.property] = value
+
+        return data
 
 
 DEFAULT_PROPERTIES = getattr(
@@ -19,23 +34,12 @@ DEFAULT_PROPERTIES = getattr(
 
 
 class StructuredDataField(StructuredDataProperty):
-    def __init__(
-        self, field, property, *args, fallback="", callback=lambda attr: attr, **kwargs
-    ):
+    def __init__(self, field, property, *args, **kwargs):
         self.field = field
-        self.fallback = fallback
-        self.callback = callback
         super().__init__(property, *args, **kwargs)
 
     def contribute_to_class(self, cls, name):
         self.field.contribute_to_class(cls, name)
-        self.value = lambda obj: self.callback(
-            getattr(obj, name)
-            or (
-                getattr(obj, self.fallback)
-                if self.fallback and hasattr(obj, self.fallback)
-                else None
-            )
-        )
+        self.value = lambda obj: getattr(obj, name)
 
         cls.structured_data_properties.append(self)
